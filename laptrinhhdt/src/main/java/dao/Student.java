ab2b6f3d8feb58ce015584e0af030a1b75426d51
ndhcuong <ndhcuong@gmail.com>
2021-03-09 14:57:36 +0700

    Them cac thao tac csdl

--- laptrinhhdt/src/main/java/dao/Student.java
+++ laptrinhhdt/src/main/java/dao/Student.java
@@ -6,7 +6,14 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -37,31 +44,115 @@ public class Student {
     }
     
     //Xây dựng hàm kết nối đến cơ sở dữ liệu
-    public Connection lay_ket_noi_csdl(String database,
-                                        String username, 
-                                        String password){
-        //Code của bạn ở đây
+    public Connection lay_ket_noi_csdl(){
+        try {
+            //Code của bạn ở đây
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/qlht",
+                    "root", "@Dmin1234");
+            return conn;
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
     //Xây dựng hàm thêm 1 student vào database
     public void save_student_to_db(){
-        //Code của bạn ở đây
+        try {
+            //Code của bạn ở đây
+            //Buoc 1: Lay ket noi toi csdl
+            Connection ketnoicsdl = lay_ket_noi_csdl();
+            
+            // the mysql insert statement
+            String query = " insert into student (fullname, sdt, email)"
+                    + " values (?, ?, ?)";
+            
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = ketnoicsdl.prepareStatement(query);
+            preparedStmt.setString(1, this.fullname);
+            preparedStmt.setString(2, this.sdt);
+            preparedStmt.setString(3, this.email);
+            
+            // execute the preparedstatement
+            preparedStmt.execute();
+            
+            ketnoicsdl.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     //Hàm lấy ra danh sách các sinh viên trong bang student
     public ArrayList<Student> lay_danh_sach_sinh_vien(){
-        //code của bạn ở đây
+        ArrayList<Student> result = new ArrayList<>();
+        try {
+            //code của bạn ở đây
+            //Buoc 1: Tao ket noi toi csdl
+            Connection ketnoicsdl = lay_ket_noi_csdl();
+            Statement stmt=ketnoicsdl.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from student");
+            while(rs.next()){
+                Student sv = new Student(rs.getInt("idstudent"), 
+                                        rs.getString("fullname"), 
+                                        rs.getString("sdt"), 
+                                        rs.getString("email"));
+                result.add(sv);
+            }
+            ketnoicsdl.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
     
     
     //Hàm cập nhật sinh viên hiện tại vào csdl
     public void update_current_student(){
-        //code của bạn ở đây
+        try {
+            //code của bạn ở đây
+            //Buoc 1: Tao ket noi toi csdl
+            Connection ketnoicsdl = lay_ket_noi_csdl();
+            // create the java mysql update preparedstatement
+            String query = "update student set fullname = ?,"
+                    + " sdt = ?,"
+                    + "email = ? where idstudent = ?";
+            PreparedStatement preparedStmt = ketnoicsdl.prepareStatement(query);
+            preparedStmt.setString(1, this.fullname);
+            preparedStmt.setString(2, this.sdt);
+            preparedStmt.setString(3, this.email);
+            preparedStmt.setInt(4, this.idstudent);
+            
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+            
+            ketnoicsdl.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     //Hàm xóa sinh viên hiện tại ra khỏi csdl
     public void delete_current_student(){
         //code của bạn ở đây
+        try {
+            //code của bạn ở đây
+            //Buoc 1: Tao ket noi toi csdl
+            Connection ketnoicsdl = lay_ket_noi_csdl();
+            // create the java mysql update preparedstatement
+            String query = "delete from student where idstudent = ?";
+            PreparedStatement preparedStmt = ketnoicsdl.prepareStatement(query);
+            preparedStmt.setInt(1, this.idstudent);
+            
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+            
+            ketnoicsdl.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     //Các getter và setter 
